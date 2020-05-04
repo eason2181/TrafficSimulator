6360967d8ed3b5c7655c8119f2a449cb440fd806
eason <eason2181@gmail.com>
2020-05-04 17:07:10 -0400

    minor change

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -4,26 +4,26 @@
 
 /* Implementation of class "MessageQueue" */
 
-/* 
-template <typename T>
-T MessageQueue<T>::receive()
-{
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
-    // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
-}
 
-template <typename T>
-void MessageQueue<T>::send(T &&msg)
-{
-    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
-    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
-}
-*/
+//template <typename T>
+//T MessageQueue<T>::receive()
+//{
+////     FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait()
+////     to wait for and receive new messages and pull them from the queue using move semantics.
+////     The received object should then be returned by the receive function.
+//}
+//
+//template <typename T>
+//void MessageQueue<T>::send(T &&msg)
+//{
+////     FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex>
+////     as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+//}
+
 
 /* Implementation of class "TrafficLight" */
 
-/* 
+
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -55,4 +55,4 @@ void TrafficLight::cycleThroughPhases()
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
 }
 
-*/
+
