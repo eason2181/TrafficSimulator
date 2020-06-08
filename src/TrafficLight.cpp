84a21436ae36b68366d02ce23d8c7e233eef58c0
eason <eason2181@gmail.com>
2020-06-08 07:22:46 -0400

    adding comments

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -10,8 +10,6 @@
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
-	//     FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex>
-	//     as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
 	std::lock_guard<std::mutex> lock(_mtx);
 	_queue.emplace_back(std::move(msg));
 	_cond.notify_one();
@@ -20,9 +18,6 @@ void MessageQueue<T>::send(T &&msg)
 template <typename T>
 T MessageQueue<T>::receive()
 {
-//     FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait()
-//     to wait for and receive new messages and pull them from the queue using move semantics.
-//     The received object should then be returned by the receive function.
 	std::unique_lock<std::mutex> lock(_mtx);
 	_cond.wait(lock, [this]() {return !_queue.empty(); });
 	T res = std::move(_queue.back());
@@ -40,9 +35,6 @@ TrafficLight::TrafficLight()
 
 void TrafficLight::waitForGreen()
 {
-    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
-    // runs and repeatedly calls the receive function on the message queue. 
-    // Once it receives TrafficLightPhase::green, the method returns.
     while (true){
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
         auto msg = _messageQ->receive();
@@ -59,7 +51,6 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class.
 	threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
